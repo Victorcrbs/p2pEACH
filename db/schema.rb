42b6345b3260ac2f684defb20a44911038b542cd
Ana Clara Cavalcante <anaclaracavalcante14@gmail.com>
2020-11-08 23:01:20 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_225830) do
+ActiveRecord::Schema.define(version: 2020_11_08_230100) do
 
   create_table "anuncios", force: :cascade do |t|
     t.string "item"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_08_225830) do
     t.string "tags"
     t.string "picture"
     t.string "tipo"
+    t.string "estado"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
