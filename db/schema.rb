c1aa3ea399cf0deaa450161fe32571f93898774b
Ana Clara Cavalcante <anaclaracavalcante14@gmail.com>
2020-11-23 05:20:41 -0300

    Adicionando field de 'tipo' em anuncio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_040203) do
+ActiveRecord::Schema.define(version: 2020_11_23_081839) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_040203) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "usuario_id", null: false
+    t.string "tipo", null: false
     t.index ["usuario_id"], name: "index_anuncios_on_usuario_id"
   end
 
