27f5479c3d6735d34bde6e3fe6e16955412c7766
15483114 <15483114.ik@usp.br>
2020-12-06 11:26:49 -0300

    Delete branch

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,5 +4,8 @@ class CommentsController < ApplicationController
         @comment = @anuncio.comments.create(params[:comment].permit(:name, :body))
         redirect_to anuncio_path(@anuncio)
     end
-   
+    def new
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.new(parent_id: params[:parent_id])
+      end
 end
